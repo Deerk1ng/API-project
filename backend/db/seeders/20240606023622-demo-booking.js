77cc9bb2e740232e2a2da5c50610d0e43b31e382
Hayden Ruiz <haydenr1111@gmail.com>
2024-06-09 00:53:17 -0700

    ??

--- backend/db/seeders/20240606023622-demo-booking.js
+++ backend/db/seeders/20240606023622-demo-booking.js
@@ -4,11 +4,12 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 };
 const data = [{
-//   "spotId": 1,
-//   "userId": 2,
-//   "startDate": "2021-11-19",
-//   "endDate": "2021-11-20",
-// },{
+  "spotId": 1,
+  "userId": 2,
+  "startDate": "2021-11-19",
+  "endDate": "2021-11-20",
+// },
+// {
 // "spotId": 2,
 // "userId": 3,
 // "startDate": "2021-12-19",
