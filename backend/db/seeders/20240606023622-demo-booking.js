cd871e959f61edfecd65c9e8597ac25a5d549d8f
Hayden Ruiz <haydenr1111@gmail.com>
2024-06-09 00:46:46 -0700

    ???

--- backend/db/seeders/20240606023622-demo-booking.js
+++ backend/db/seeders/20240606023622-demo-booking.js
@@ -3,17 +3,17 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 };
-const data = [{
-  "spotId": 1,
-  "userId": 2,
-  "startDate": "2021-11-19",
-  "endDate": "2021-11-20",
-},{
-"spotId": 2,
-"userId": 3,
-"startDate": "2021-12-19",
-"endDate": "2021-12-20"
-}]
+// const data = [{
+//   "spotId": 1,
+//   "userId": 2,
+//   "startDate": "2021-11-19",
+//   "endDate": "2021-11-20",
+// },{
+// "spotId": 2,
+// "userId": 3,
+// "startDate": "2021-12-19",
+// "endDate": "2021-12-20"
+// }]
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
