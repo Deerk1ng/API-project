c4fb27627ebc429f67bb1aaf57a265226353b26b
Hayden Ruiz <haydenr1111@gmail.com>
2024-07-25 23:41:08 -0700

    lots of css completed. new icon uploaded

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
-import { FaUserCircle } from 'react-icons/fa';
+import { FaUserCircle, FaAlignJustify } from 'react-icons/fa';
 import * as sessionActions from '../../store/session';
 import './Navigation.css'
-import OpenModalButton from '../OpenModalButton/OpenModalButton';
 import LoginFormModal from '../LoginFormModal/LoginFormModal';
 import SignupFormModal from '../SignupFormModal/SignupFormModal';
+import OpenModalMenuItem from '../OpenModalMenuItem/OpenModalMenuItem';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
@@ -44,32 +44,31 @@ function ProfileButton({ user }) {
 
   return (
     <>
-      <button onClick={toggleMenu}>
-        <FaUserCircle />
+      <button className='user-button' onClick={toggleMenu}>
+        <FaAlignJustify />  <FaUserCircle />
       </button>
       <ul className={ulClassName} ref={ulRef}>
         { user ? (
           <>
-            <li>{user.username}</li>
-            <li>{user.firstName} {user.lastName}</li>
+            <li>Hello, {user.username}</li>
             <li>{user.email}</li>
             <li>
-              <button onClick={logout}>Log Out</button>
+              <button className='logout-button' onClick={logout}>Log Out</button>
             </li>
           </>
           ) : (
             <>
               <li>
-                <OpenModalButton
-                  buttonText="Log In"
-                  onButtonClick={closeMenu}
+                <OpenModalMenuItem
+                  itemText="Log In"
+                  onItemClick={closeMenu}
                   modalComponent={<LoginFormModal />}
                 />
               </li>
               <li>
-                <OpenModalButton
-                  buttonText="Sign Up"
-                  onButtonClick={closeMenu}
+                <OpenModalMenuItem
+                  itemText="Sign Up"
+                  onItemClick={closeMenu}
                   modalComponent={<SignupFormModal />}
                 />
               </li>
--- frontend/src/components/SpotsPage/Spots.jsx
+++ frontend/src/components/SpotsPage/Spots.jsx
@@ -1,14 +1,14 @@
 import './SpotsPage.css'
-
+import { FaStar } from 'react-icons/fa'
 
 const Spots = (spot) => {
     spot = spot.spots
     return (
-        <div className="spot-card">
+        <div title={spot.name} className="spot-card">
             <img src={spot.previewImage} alt="preview of spot" className="prevImg"/>
             <div className='spot-preview'>
                 <div className="spot-addy">{spot.city},{spot.state}</div>
-                <div className="spot-rating">{Math.round(spot.avgRating * 100) / 100}</div>
+                <div className="spot-rating"><FaStar /> {spot.avgRating > 0 ? Math.round(spot.avgRating * 100) / 100 : 'New'}</div>
             </div>
                 <div className="spot-price">${spot.price} night</div>
         </div>
